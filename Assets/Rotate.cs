59890afdf48eda6886cde7c0b3a32295f1d144c1
Christian Leon Christensen <chrleon@gmail.com>
2022-02-13 18:37:12 +0100

    We got movement!

--- Assets/Rotate.cs
+++ Assets/Rotate.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Rotate : MonoBehaviour
 {
+    [SerializeField] float speed;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,6 @@ public class Rotate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        this.transform.Rotate(Vector3.up * Time.deltaTime * speed);
     }
 }
